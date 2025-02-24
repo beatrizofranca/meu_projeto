01edc1861d16b172e0ed3bc1376361dd51a22827
Beatriz França <anabeatrizo.franca@gmail.com>
2025-02-23 23:04:32 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -4,6 +4,10 @@ int soma(int a, int b) {
     return a + b;
 }
 
+int subtracao(int a, int b) {
+    return a - b;
+}
+
 int multiplicacao(int a, int b) {
     return a * b;
 }
@@ -11,13 +15,20 @@ int multiplicacao(int a, int b) {
 int main() {
     int a, b;
 
+    // Solicita e lê os dois números
     printf("Digite o primeiro número: ");
     scanf("%d", &a);
 
     printf("Digite o segundo número: ");
     scanf("%d", &b);
 
+    // Chama a função soma e imprime o resultado
     printf("A soma de %d e %d é: %d\n", a, b, soma(a, b));
+
+    // Chama a função subtração e imprime o resultado
+    printf("A subtração de %d e %d é: %d\n", a, b, subtracao(a, b));
+
+    // Chama a função multiplicação e imprime o resultado
     printf("A multiplicação de %d e %d é: %d\n", a, b, multiplicacao(a, b));
 
     return 0;
