39d321a5826cf410952ce7129a859f15de9a5a5d
Beatriz França <anabeatrizo.franca@gmail.com>
2025-02-23 21:54:46 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -1,1 +1,28 @@
-"#include <stdio.h>\n\nint main() {\n    int a, b, soma;\n\n    printf(\"Digite o primeiro número: \");\n    scanf(\"%d\", &a);\n\n    printf(\"Digite o segundo número: \");\n    scanf(\"%d\", &b);\n\n    soma = a + b;\n\n    printf(\"A soma de %d e %d é: %d\\n\", a, b, soma);\n\n    return 0;\n}" 
+#include <stdio.h>
+
+int soma(int a, int b) {
+    return a + b;
+}
+
+int subtracao(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    int a, b;
+
+    // Solicita e lê os dois números
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+
+    // Chama a função soma e imprime o resultado
+    printf("A soma de %d e %d é: %d\n", a, b, soma(a, b));
+
+    // Chama a função subtração e imprime o resultado
+    printf("A subtração de %d e %d é: %d\n", a, b, subtracao(a, b));
+
+    return 0;
+}
